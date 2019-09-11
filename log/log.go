ac7b4d2d4b0a9a52b6754617dbf88d496027c451
Tej Chajed <tchajed@mit.edu>
2019-09-11 10:37:09 -0400

    Add skeleton of log API

---
+++ log/log.go
@@ -0,0 +1,39 @@
+package log
+
+import "../disk"
+
+type Log struct {}
+
+// MaxTxnWrites is a guaranteed reservation for each transaction.
+const MaxTxnWrites = 10 // 10 is completely arbitrary
+
+// BeginTxn allocates space for a new transaction in the log.
+//
+// Returns true if the allocation succeeded.
+func (l Log) BeginTxn() bool {
+	return true
+}
+
+// Read from the logical disk.
+//
+// Reads must go through the log to return committed but un-applied writes.
+func (l Log) Read(a int) disk.Block {
+	panic("unimplemented")
+}
+
+// Write to the disk through the log.
+func (l Log) Write(a int, v disk.Block) {
+	panic("unimplemented")
+}
+
+// Commit the current transaction.
+func (l Log) Commit() {
+	panic("unimplemented")
+}
+
+// Apply all the committed transactions.
+//
+// Frees all the space in the log.
+func (l Log) Apply() {
+	panic("unimplemented")
+}
