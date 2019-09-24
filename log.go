d25622c55ef1337463b01af789a804d6a5564bbb
Tej Chajed <tchajed@mit.edu>
2019-09-24 14:55:40 -0400

    Encode log addresses into a single block

--- log.go
+++ log.go
@@ -11,11 +11,18 @@ import (
 // MaxTxnWrites is a guaranteed reservation for each transaction.
 const MaxTxnWrites uint64 = 10 // 10 is completely arbitrary
 
-const logLength = 1 + 2*MaxTxnWrites
+// assumes MaxTwnWrites addresses fit into one block;
+// this holds when MaxTxnWrites <= 4096/8 = 512
+// (actually the total number of writes in the log needs to be <= 512,
+// which would be different if we supported group commit of multiple
+// transactions)
+
+const logLength = 1 + 1 + MaxTxnWrites
 
 type Log struct {
 	// protects cache and length
 	l     *sync.RWMutex
+	addrs *[]uint64
 	cache map[uint64]disk.Block
 	// length of current transaction, in blocks
 	length *uint64
@@ -38,13 +45,17 @@ func New() Log {
 	if diskSize <= logLength {
 		panic("disk is too small to host log")
 	}
+
+	addrs := make([]uint64, 0)
+	addrPtr := new([]uint64)
+	*addrPtr = addrs
 	cache := make(map[uint64]disk.Block)
 	header := intToBlock(0)
 	disk.Write(0, header)
 	lengthPtr := new(uint64)
 	*lengthPtr = 0
 	l := new(sync.RWMutex)
-	return Log{cache: cache, length: lengthPtr, l: l}
+	return Log{l: l, addrs: addrPtr, cache: cache, length: lengthPtr}
 }
 
 func (l Log) lock() {
@@ -86,7 +97,7 @@ func (l Log) Read(a uint64) disk.Block {
 }
 
 func (l Log) Size() uint64 {
-	// safe to do lock-free
+	// atomic, so safe to do lock-free
 	sz := disk.Size()
 	return sz - logLength
 }
@@ -98,38 +109,74 @@ func (l Log) Write(a uint64, v disk.Block) {
 	if length >= MaxTxnWrites {
 		panic("transaction is at capacity")
 	}
-	aBlock := intToBlock(a)
-	nextAddr := 1 + 2*length
-	disk.Write(nextAddr, aBlock)
+	nextAddr := 1 + 1 + length
+	addrs := *l.addrs
+	newAddrs := append(addrs, a)
+	*l.addrs = newAddrs
 	disk.Write(nextAddr+1, v)
 	l.cache[a] = v
 	*l.length = length + 1
 	l.unlock()
 }
 
+// encodeAddrs produces a disk block that encodes the addresses in the log
+func (l Log) encodeAddrs() disk.Block {
+	length := *l.length
+	addrs := *l.addrs
+	aBlock := make([]byte, 4096)
+	for i := uint64(0); ; {
+		if i < length {
+			ai := addrs[i]
+			machine.UInt64Put(aBlock[i*8:(i+1)*8], ai)
+			i = i + 1
+			continue
+		}
+		break
+	}
+	return aBlock
+}
+
+// decodeAddrs reads the address disk block and decodes it into length addresses
+func decodeAddrs(length uint64) []uint64 {
+	addrs := make([]uint64, length)
+	aBlock := disk.Read(1)
+	for i := uint64(0); ; {
+		if i < length {
+			a := machine.UInt64Get(aBlock[i*8 : (i+1)*8])
+			addrs[i] = a
+			i = i + 1
+			continue
+		}
+		break
+	}
+	return addrs
+}
+
 // Commit the current transaction.
 func (l Log) Commit() {
 	l.lock()
 	length := *l.length
+	aBlock := l.encodeAddrs()
 	// TODO: maybe safe to unlock early?
 	l.unlock()
+	disk.Write(1, aBlock)
 	header := intToBlock(length)
 	disk.Write(0, header)
 }
 
-func getLogEntry(logOffset uint64) (uint64, disk.Block) {
-	diskAddr := 1 + 2*logOffset
-	aBlock := disk.Read(diskAddr)
-	a := blockToInt(aBlock)
+func getLogEntry(addrs []uint64, logOffset uint64) (uint64, disk.Block) {
+	diskAddr := 1 + 1 + logOffset
+	a := addrs[logOffset]
 	v := disk.Read(diskAddr + 1)
 	return a, v
 }
 
 // applyLog assumes we are running sequentially
-func applyLog(length uint64) {
+func applyLog(addrs []uint64) {
+	length := uint64(len(addrs))
 	for i := uint64(0); ; {
 		if i < length {
-			a, v := getLogEntry(i)
+			a, v := getLogEntry(addrs, i)
 			disk.Write(logLength+a, v)
 			i = i + 1
 			continue
@@ -147,8 +194,10 @@ func clearLog() {
 // Frees all the space in the log.
 func (l Log) Apply() {
 	l.lock()
-	length := *l.length
-	applyLog(length)
+	addrs := *l.addrs
+	applyLog(addrs)
+	newAddrs := make([]uint64, 0)
+	*l.addrs = newAddrs
 	clearLog()
 	*l.length = 0
 	l.unlock()
@@ -161,12 +210,15 @@ func (l Log) Apply() {
 func Open() Log {
 	header := disk.Read(0)
 	length := blockToInt(header)
-	applyLog(length)
+	addrs := decodeAddrs(length)
+	addrPtr := new([]uint64)
+	*addrPtr = addrs
+	applyLog(addrs)
 	clearLog()
 
 	cache := make(map[uint64]disk.Block)
 	lengthPtr := new(uint64)
 	*lengthPtr = 0
 	l := new(sync.RWMutex)
-	return Log{cache: cache, length: lengthPtr, l: l}
+	return Log{l: l, addrs: addrPtr, cache: cache, length: lengthPtr}
 }
